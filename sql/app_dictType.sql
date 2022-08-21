93c0e5a51992df8ddc5fefb2cfce6f787e533f1f
皮皮大刺猬 <1125373330@qq.com>
2022-08-21 21:22:23 +0800

    同步若依代码

--- sql/app_dictType.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `sys_dict_type ` VALUES (11, '用户类型', 'demo_user_type', '0', 'admin', '2022-6-18 13:46:18', '', NULL, NULL);
-INSERT INTO `sys_dict_type ` VALUES (12, '会员标签', 'demo_user_label', '0', 'admin', '2022-6-18 13:50:46', '', NULL, NULL);
