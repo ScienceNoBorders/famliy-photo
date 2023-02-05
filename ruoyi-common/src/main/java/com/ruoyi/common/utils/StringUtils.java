c1f300db8e4be2e869ad678f81af358235889028
皮皮大刺猬 <1125373330@qq.com>
2023-02-05 23:40:24 +0800

    字符未使用下划线不进行驼峰式处理

--- ruoyi-common/src/main/java/com/ruoyi/common/utils/StringUtils.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/utils/StringUtils.java
@@ -1,14 +1,10 @@
 package com.ruoyi.common.utils;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import org.springframework.util.AntPathMatcher;
 import com.ruoyi.common.constant.Constants;
 import com.ruoyi.common.core.text.StrFormatter;
+import org.springframework.util.AntPathMatcher;
+
+import java.util.*;
 
 /**
  * 字符串工具类
@@ -481,13 +477,18 @@ public class StringUtils extends org.apache.commons.lang3.StringUtils
     }
 
     /**
-     * 驼峰式命名法 例如：user_name->userName
+     * 驼峰式命名法
+     * 例如：user_name->userName
      */
     public static String toCamelCase(String s)
     {
         if (s == null)
         {
             return null;
+        }
+        if (s.indexOf(SEPARATOR) == -1)
+        {
+            return s;
         }
         s = s.toLowerCase();
         StringBuilder sb = new StringBuilder(s.length());
